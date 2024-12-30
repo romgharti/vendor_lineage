c3a912de77fe092f631759ee50618b9d304a76d2
Arman <84268732+Arman-ATI@users.noreply.github.com>
2024-12-30 23:11:09 +0330

    [TMP] remove the conflict with our gms repo

--- config/common.mk
+++ config/common.mk
@@ -243,7 +243,6 @@ endif
 
 # SetupWizard
 PRODUCT_PRODUCT_PROPERTIES += \
-    setupwizard.theme=glif_v4 \
     setupwizard.feature.day_night_mode_enabled=true
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/no-rro
