ProjectEverest/vendor_everest

vendor: Import our Bootanimation
Special thanks to:
@SudipRajbanshi - for logo
@ztrahmet - for animation
@XelXen - for fixes

config/bootanimation.mk
@@ -1,0 +1,15 @@
+# Bootanimation
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/everest/bootanimation/bootanimation-1080p.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/everest/bootanimation/bootanimation-1440p.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/everest/bootanimation/bootanimation-720p.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += vendor/everest/bootanimation/bootanimation-1080p.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif

config/common.mk
@@ -62,6 +62,9 @@ SYSTEMUI_OPTIMIZE_JAVA ?= true
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# Bootanimation
+include vendor/everest/config/bootanimation.mk
+
 # Call Recording
 TARGET_CALL_RECORDING_SUPPORTED ?= true
 ifneq ($(TARGET_CALL_RECORDING_SUPPORTED),false)

romgharti/vendor_lineage

config/bootanimation.mk
@@ -1,0 +1,15 @@
+# Bootanimation
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation-1080p.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation-1440p.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation-720p.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation-1080p.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif

config/common.mk
@@ -270,3 +270,6 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 include vendor/lineage/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
+
+# Bootanimation
+include vendor/lineage/config/bootanimation.mk